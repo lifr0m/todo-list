588e8fc234ca878a0f949a56bf7f1b0263c68164
Lifr0m <artem.kay@yandex.ru>
2024-08-31 12:42:36 +0300

    optimize add item on enter

--- src/components/TodoAddItem.tsx
+++ src/components/TodoAddItem.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef, Dispatch, SetStateAction, KeyboardEvent } from 'react'
+import { FC, useRef, Dispatch, SetStateAction } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { ITask } from '@/shared/types/task'
@@ -13,7 +13,7 @@ export const TodoAddItem: FC<IParams> = ({ tasks, setTasks }) => {
 
   const addTask = (): void => {
     const text = inputRef.current!.value.trim()
-    
+
     if (!text) return
 
     if (tasks.some((task) => task.text === text)) return
@@ -24,16 +24,12 @@ export const TodoAddItem: FC<IParams> = ({ tasks, setTasks }) => {
     })
   }
 
-  const keyAddTask = (event: KeyboardEvent<HTMLInputElement>): void => {
-    if (event.key == 'Enter') addTask()
-  }
-
   return (
     <div className='flex mb-4'>
       <Input
         ref={inputRef}
         placeholder='Enter a task'
-        onKeyDown={keyAddTask}
+        onKeyDown={(event) => event.key === 'Enter' && addTask()}
         className='mr-2'
       />
       <Button onClick={addTask}>Add</Button>
